5e2d608b187bf90b1401920322f6b40d5381a3bf
Jinx <jinxlovejinx@vip.qq.com>
2017-06-28 17:24:51 +0800

    http

---
+++ http_1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jinx on 6/28/17.
+ */
+
+var http = require('http');
+var url = require('url');
+var util = require('util');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end(util.inspect(url.parse(req.url, true)));
+}).listen(3000);
