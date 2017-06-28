f3a2f71b675420c046ef47b0fdc073e842c9169b
Jinx <jinxlovejinx@vip.qq.com>
2017-06-28 16:14:46 +0800

    文件流

--- stream_1.js
+++ stream_1.js
@@ -6,7 +6,6 @@ var data = '';
 
 // 创建可读流
 var readerStream = fs.createReadStream('input.txt');
-
 // 设置编码为 utf8。
 readerStream.setEncoding('UTF8');
 
@@ -14,11 +13,9 @@ readerStream.setEncoding('UTF8');
 readerStream.on('data', function(chunk) {
     data += chunk;
 });
-
 readerStream.on('end',function(){
     console.log(data);
 });
-
 readerStream.on('error', function(err){
     console.log(err.stack);
 });
